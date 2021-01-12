156d9086d5d6a1b755c469b2676c6d91e18c1f83
Mathis <mathisdesnoes@gmail.com>
2021-01-12 12:05:57 +0100

    menu nabigation puzzle

--- app/src/main/java/com/univangers/m2acdi/desnoes/puzzleresolver/MainActivity.java
+++ app/src/main/java/com/univangers/m2acdi/desnoes/puzzleresolver/MainActivity.java
@@ -1,8 +1,16 @@
 package com.univangers.m2acdi.desnoes.puzzleresolver;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openActivityPuzzle1(View v) {
+        //Intent ActivityPuzzle1 = new Intent(this, ???????????.class);
+        //startActivity(ActivityPuzzle1);
+    }
+
+    public void openActivityPuzzle2(View v) {
+        //Intent ActivityPuzzle2 = new Intent(this, ???????????.class);
+        //startActivity(ActivityPuzzle2);
+    }
+
+    public void openActivityPuzzle3(View v) {
+        //Intent ActivityPuzzle3 = new Intent(this, ???????????.class);
+        //startActivity(ActivityPuzzle3);
     }
 }
